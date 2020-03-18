1785dbb99ea2b232e382ef61f6344fd3733871ef
sztanderap <sztandera.p@gmail.com>
2020-03-18 17:03:16 +0000

    Initial Commit

---
+++ src/main/java/com/psh3mo/springinactionpart3/SpringInActionPart3Application.java
@@ -0,0 +1,13 @@
+package com.psh3mo.springinactionpart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActionPart3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInActionPart3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/psh3mo/springinactionpart3/SpringInActionPart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psh3mo.springinactionpart3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionPart3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
